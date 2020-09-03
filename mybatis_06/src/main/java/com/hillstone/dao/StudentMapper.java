16c72c69eec0521993738388dae99a9a46ec55eb
QIANG.DUAN <duanqiang1024@163.com>
2020-09-03 22:15:54 +0800

    多对一

--- mybatis_06/src/main/java/com/hillstone/dao/StudentMapper.java
+++ mybatis_06/src/main/java/com/hillstone/dao/StudentMapper.java
@@ -1,4 +1,11 @@
 package com.hillstone.dao;
 
+import com.hillstone.pojo.Student;
+
+import java.util.List;
+
 public interface StudentMapper {
+
+    //查询所有学生信息，以及对应的老师的信息！
+    List<Student> getStudent();
 }
--- mybatis_06/src/test/java/com/hillstone/dao/MyTest.java
+++ mybatis_06/src/test/java/com/hillstone/dao/MyTest.java
@@ -1,20 +1,37 @@
 package com.hillstone.dao;
 
 
+import com.hillstone.pojo.Student;
 import com.hillstone.pojo.Teacher;
 import com.hillstone.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+
 public class MyTest {
 
-@Test
-    public void getTeacher(){
-    SqlSession sqlSession = MybatisUtils.getSqlSession();
-    TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
-    Teacher teacher = teacherMapper.getTeacher(1);
-    System.out.println(teacher);
+    @Test
+    public void getTeacher() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
+        Teacher teacher = teacherMapper.getTeacher(1);
+        System.out.println(teacher);
 
-    sqlSession.close();
-}
+        sqlSession.close();
+    }
+
+    @Test
+    public void getStudent() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> studentList = studentMapper.getStudent();
+
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+
+        sqlSession.close();
+    }
+
 }
